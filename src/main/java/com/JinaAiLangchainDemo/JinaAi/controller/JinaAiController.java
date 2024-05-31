280886cb9aab72dee80b9b418a8c7b5daae33e8d
lucifer-hell <luciferhell58@gmail.com>
2024-05-31 21:41:50 +0530

    made controller changes

--- src/main/java/com/JinaAiLangchainDemo/JinaAi/controller/JinaAiController.java
+++ src/main/java/com/JinaAiLangchainDemo/JinaAi/controller/JinaAiController.java
@@ -5,19 +5,18 @@ import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.model.output.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
 @RestController
+@RequestMapping("/jina-ai")
 public class JinaAiController {
     @Autowired
     JinaAiService jinaAiService;
     @PostMapping("/get-embeddings")
-    public ResponseEntity<Embedding> getEmbeddingForInput(@RequestBody HashMap<String,String> body){
-        return ResponseEntity.ok(jinaAiService.getEmbeddingsFromInput(body.get("input")));
+    public ResponseEntity<float[]> getEmbeddingForInput(@RequestBody HashMap<String,String> body){
+        Embedding embeddingRepsone = jinaAiService.getEmbeddingsFromInput(body.get("input"));
+        return ResponseEntity.ok(embeddingRepsone.vector());
     }
 }
